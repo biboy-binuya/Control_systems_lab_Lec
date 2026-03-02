2f7de535227c4de0caa57dc74f70d39d555265b7
Jguapo-41 <abellanosaharvey25@gmail.com>
2026-03-02 18:34:18 +0800

    Correct serial command handling for LED control
    Fix conditional statement and indentation in loop function.

--- Activity_6/01_Python_and_Arduino_Activity.ino
+++ Activity_6/01_Python_and_Arduino_Activity.ino
@@ -1,19 +1,19 @@
+#include <Arduino.h>
 const int ledPin = 13;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available() > 0); {
-    char command = Serial.read();
+    if (Serial.available() > 0) {
+        char command = Serial.read();
 
-    if (command == '1'){
-      digitalWrite(ledPin, HIGH);
+        if (command == '1') {
+            digitalWrite(ledPin, HIGH);
+        } else if (command == '0') {
+            digitalWrite(ledPin, LOW);
+        }
     }
-    else if (command == '0') {
-      digitalWrite(ledPin, LOW);
-    } 
-  }
 }
